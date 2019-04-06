de32775742a792382a6a729c5861e3b3a547c5ff
irieSourceTree <wangqiang@outlook.jp>
2019-04-06 16:37:12 +0900

    mainのJavaファイル

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  
+
+    public static void main(String[] args) {
+    
+
+    // 12を3で割った値を出力してください
+    
+    System.out.println(12/3);
+    
+    
+
+    // 3に6を掛けた値を出力してください
+     
+    System.out.println(3*6);
+
+    
+  
+
+    }
+
+
+}
